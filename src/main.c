2b721c617d9c429e213197c7ab58b29c4771ac62
unknown <One>
2022-09-06 16:54:22 +0900

    Add led code in main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "init_base.h"
 
 volatile int i = 0;
+uint8_t LED1=1, LED2=1, LED3=1, LED4=1;
 
 int main(void)
 {
@@ -14,11 +15,23 @@ int main(void)
 	init_INTC();
 	init_Linflex0();
 	
+	/* Pin Setting */
+	SIU.PCR[52].R = 0x0200;  // LED1
+	SIU.PCR[53].R = 0x0200;  // LED2
+	SIU.PCR[54].R = 0x0200;  // LED3
+	SIU.PCR[55].R = 0x0200;  // LED4
+	
 	/* Loop forever */
 	for (;;) 
 	{
 		FMSTR_Recorder();
 		FMSTR_Poll();
+		
+		/* LED ON */
+		SIU.GPDO[52].B.PDO = LED1;
+		SIU.GPDO[53].B.PDO = LED2;
+		SIU.GPDO[54].B.PDO = LED3;
+		SIU.GPDO[55].B.PDO = LED4;
 		
 		i++;
 	}
